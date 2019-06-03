ba241d003083db5cf55b881ab9fa164b962bb41d
SlavnicN <slavnic.n@gmail.com>
2019-06-03 08:03:31 +0200

    creating first contract

---
+++ contest/contracts/Contest.sol
@@ -0,0 +1,14 @@
+pragma solidity 0.4.24;
+
+//creating the contract 
+contract Constest{
+
+	//declaring variables
+	string public contestant;
+
+
+	// declaring constructor
+	function Contest() public {
+		contestant = "Tom";		
+	} 
+}
