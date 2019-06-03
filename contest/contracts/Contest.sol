97ee0ea85aefe34fd1c03169061619d619097ca8
SlavnicN <slavnic.n@gmail.com>
2019-06-03 08:34:28 +0200

    multiple contestants struct

--- contest/contracts/Contest.sol
+++ contest/contracts/Contest.sol
@@ -3,12 +3,22 @@ pragma solidity 0.5.8;
 //creating the contract 
 contract Contest{
 
-	//declaring variables
-	string public contestant;
+	// creating strucre to model the contestant
+	struct Contestant {
+		uint id; 
+		string name; 
+		uint voteCount; 		
+	}
 
+	// use mapping to get or fetch the contestant details
+	mapping(uint => contestant) public contestants;
 
-	// declaring constructor
-	constructor() public {
-		contestant = "Tom";		
-	} 
+	// add a public state variable to keep track of contestant Count 
+	uint public contestantsCount; 
+
+	// add a function to add contestant
+	function addContestant(string _name) private {
+		contestantsCount ++; 
+		contestants[contestantsCount] = Contestant(contestantsCount, _name, 0);		
+	}
 }
