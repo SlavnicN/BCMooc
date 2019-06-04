54164925f65e14195e132b7eed3ae63adcb9f636
SlavnicN <slavnic.n@gmail.com>
2019-06-04 07:49:48 +0200

    Add test function

--- contest/contracts/Contest.sol
+++ contest/contracts/Contest.sol
@@ -11,13 +11,18 @@ contract Contest{
 	}
 
 	// use mapping to get or fetch the contestant details
-	mapping(uint => contestant) public contestants;
+	mapping(uint => Contestant) public contestants;
 
 	// add a public state variable to keep track of contestant Count 
 	uint public contestantsCount; 
 
+	constructor() public {
+		addContestant("Tom");
+		addContestant("Jerry");
+
+	}
 	// add a function to add contestant
-	function addContestant(string _name) private {
+	function addContestant(string memory _name) private {
 		contestantsCount ++; 
 		contestants[contestantsCount] = Contestant(contestantsCount, _name, 0);		
 	}
